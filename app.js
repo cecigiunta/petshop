701231d350b0a6fdc3bf876ff3c82a69bc4f3fb8
cecigiunta <ceciliagiunta57@gmail.com>
2022-08-05 18:53:03 -0300

    paginas y js con fetch

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-//holss
+Vue.createApp({
+    data() {
+        return {
+            url: 'https://apipetshop.herokuapp.com/api/articulos',
+            results: [],
+            juguetes: [],
+            farmacia: [],   
+
+        
+         
+            
+    }},
+    created(){
+            fetch(this.url)
+            .then((res) => res.json())
+            .then((data) => {
+                this.results = data.response;
+                console.log(this.results);
+            })
+            .catch(err => console.error(err))
+    },
+    methods: {
+
+
+
+
+
+    },
+    computed: {
+
+        },            
+}).mount('#app')

485e8080df37cec0ade7d543e0f3e90200b7f677
Scottti <yamiscotti@gmail.com>
2022-08-10 20:41:25 -0300

    2da version

--- farmacia.js
+++ farmacia.js
@@ -4,26 +4,43 @@ Vue.createApp({
             url: 'https://apipetshop.herokuapp.com/api/articulos',
             results: [],
             juguetes: [],
-            farmacia: [],   
-   
+            farmacia: [],  
+            range: 0,
+            preciosFiltrados: []
+            
     }},
     created(){
             fetch(this.url)
             .then((res) => res.json())
             .then((data) => {
                 this.results = data.response;
+                this.preciosFiltrados = data.response.sort((a, b) => {
+                    return a.stock - b.stock;
+                }); // ?????
                 console.log(this.results);
             })
             .catch(err => console.error(err))
     },
     methods: {
-
-
+        filtrarPrecios: function(){
+                this.preciosFiltrados = this.farmacia.filter(item => item.precio <= this.range)
+                this.preciosFiltrados.sort((a, b) => {
+                    return a.stock - b.stock;
+                });
+                console.log(this.preciosFiltrados)
+            }
     },
     computed: {
         filtrarMedicamentos: function(){
             this.farmacia = this.results.filter((medicamento) => medicamento.tipo === "Medicamento")
+            this.farmacia.sort((a, b) => {
+                return a.stock - b.stock;
+            });
+        },
+        cambiar: function(){
+            return this.range
+        },
+        
         }
-
-        },            
+           
 }).mount('#app')

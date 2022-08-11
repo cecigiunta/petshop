26ba2a158b6db7a4008378b9156d6bdb9bbca833
caro-sosa-s <sosasalgueroc@gmail.com>
2022-08-11 13:03:08 -0300

    agrego favs a farmacia

--- juguetes.js
+++ juguetes.js
@@ -8,12 +8,14 @@ Vue.createApp({
       
     }
   },
-  mounted() {
+  created() {
     fetch("https://apipetshop.herokuapp.com/api/articulos")
       .then((res) => res.json())
       .then((data) => {
         this.results = Array.from(data.response);
+        this.agregarPropiedades();
         this.getJuguetes();
+        
         
       })
       .catch(error => console.log(error));
@@ -22,40 +24,38 @@ Vue.createApp({
   methods: {
     agregarPropiedades: function() {
       for(item of this.results) {
-        if(!this.favoritos?.some(item => item.nombre === juguete.nombre)) {
-          juguete.agregar = true;
-          juguete.id = juguete.nombre.replace(/ /g, "_").toLowerCase();
-          juguete.idtag = "#"+juguete.nombre;
-          juguete.idtag = juguete.idtag.replace(/ /g, "_").toLowerCase()
+        if(!this.favoritos?.some(favorito => favorito.nombre === item.nombre)) {
+          item.agregar = true;
+          item.id = item.nombre.replace(/ /g, "_").toLowerCase();
+          item.idtag = "#"+item.nombre;
+          item.idtag = item.idtag.replace(/ /g, "_").toLowerCase()
           
         } else {
-          juguete.agregar = false;
-          juguete.id = juguete.nombre.replace(/ /g, "_").toLowerCase();
-          juguete.idtag= "#"+juguete.nombre;
-          juguete.idtag = juguete.idtag.replace(/ /g, "_").toLowerCase()
+          item.agregar = false;
+          item.id = item.nombre.replace(/ /g, "_").toLowerCase();
+          item.idtag= "#"+item.nombre;
+          item.idtag = item.idtag.replace(/ /g, "_").toLowerCase()
         }
       
       }
     },
-
     getJuguetes: function () {
         this.juguetes = this.results.filter(item => item.tipo === "Juguete").sort((a,b)=> a.stock - b.stock)
     },
 
-    agregarFavorito : function(juguete) {
-     if(!this.favoritos?.some(item => item.nombre === juguete.nombre)) {
-        this.favoritos?.push(juguete);
+    agregarFavorito : function(item) {
+     if(!this.favoritos?.some(favorito=> favorito.nombre === item.nombre)) {
+        this.favoritos?.push(item);
         console.log(this.favoritos);
-        juguete.agregar = false;
+        item.agregar = false;
         localStorage.setItem('favoritos', JSON.stringify(this.favoritos));
          
      }
      this.condicionFav = false;
     },
-
-    quitarFavorito : function(juguete) {
-        juguete.agregar = true;
-        this.favoritos = this.favoritos?.filter(j => j.nombre !== juguete.nombre)
+    quitarFavorito : function(item) {
+        item.agregar = true;
+        this.favoritos = this.favoritos?.filter(j => j.nombre !== item.nombre)
         this.condicionFav = true;
         localStorage.setItem('favoritos', JSON.stringify(this.favoritos));
     }
